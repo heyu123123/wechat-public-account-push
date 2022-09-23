7eb986a59396bf5309be038fd7f62264d3c00f79
heyu123123 <113917671+heyu123123@users.noreply.github.com>
2022-09-24 00:15:51 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -64,7 +64,7 @@ export const USER_CONFIG = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "opvt-514DIyeuKV14dkiQ-ijFuuc",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "hzdLTbqOBT7H71flUhh3OcDvTAsT0-65lv3mZpX5k4Y",
+      useTemplateId: "RY-uVEGmsZqJKs9w-5XPidzK9SKSLrd2hx2K7mpaLrc",
       // 所在省份
       province: "山东",
       // 所在城市
